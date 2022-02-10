44b9bc12d52454e3c2881d52489ff0f907693edc
Dusan Kovacevic <dusan.kovacevic@fishingbooker.com>
2022-02-10 17:19:14 +0100

    hotfix

--- tests/Transactions/test_devsuite.py
+++ tests/Transactions/test_devsuite.py
@@ -8,7 +8,9 @@ from helpers.app_helpers import sender_use_seconds, create_recipient_and_sender_
     sender_use_days, sender_use_weeks, sender_use_months, sender_use_years, sender_create_contract, \
     sender_top_up_while_streaming, sender_top_up_before_stream_started, sender_cancel_contract, \
     recipient_can_cancel_and_transfer, sender_transfer_contract, recipient_cancel_contract, recipient_transfer_contract,\
-    recipient_withdraw_partial, recipient_withdraw_full, non_can_cancel_and_transfer, reconnect_sender
+    recipient_withdraw_partial, recipient_withdraw_full, non_can_cancel_and_transfer, reconnect_sender, \
+    create_recipient_and_sender_fill_details_for_vesting, sender_use_random_date_and_time, \
+    sender_create_contract_and_recipient_assert_contract_vesting, set_random_cliff
 
 
 @pytest.mark.devs
