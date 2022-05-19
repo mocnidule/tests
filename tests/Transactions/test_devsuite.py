f6cd6fba889d488426b1e67851e468898aacd030
Dusan Kovacevic <dusankovacevic@Dusans-MacBook-Pro-2.local>
2022-05-19 15:34:48 +0200

    uncomment

--- tests/Transactions/test_devsuite.py
+++ tests/Transactions/test_devsuite.py
@@ -24,47 +24,46 @@ class test_dev(unittest.TestCase):
         click_add_recipient_100_times()
         fill_batch_details()
         sender_use_random_date_and_time()
-        sleep(1000)
         sender_create_contract_and_recipient_assert_contract_vesting()
 
-    # def test_vesting_minutes(self):
-    #     sender_go_vesting_select_devnet_connect_wallet()
-    #     sender_fill_standard_contract_details()
-    #     sender_use_random_date_and_time()
-    #     sender_use_minutes()
-    #     sender_create_contract_and_recipient_assert_contract_vesting()
-    #
-    # def test_vesting_and_recipient_cancel(self):
-    #     sender_go_vesting_select_devnet_connect_wallet()
-    #     sender_fill_standard_contract_details()
-    #     sender_use_random_date_and_time()
-    #     select_both_can_cancel()
-    #     sender_create_contract_and_recipient_assert_contract_vesting()
-    #     cancel_contract()
+    def test_vesting_minutes(self):
+        sender_go_vesting_select_devnet_connect_wallet()
+        sender_fill_standard_contract_details()
+        sender_use_random_date_and_time()
+        sender_use_minutes()
+        sender_create_contract_and_recipient_assert_contract_vesting()
 
-    # def test_vesting_and_sender_set_cliff(self):
-    #     sender_go_vesting_select_devnet_connect_wallet()
-    #     sender_fill_standard_contract_details()
-    #     sender_use_random_date_and_time()
-    #     set_random_cliff()
-    #     sender_create_contract_and_recipient_assert_contract_vesting()
+    def test_vesting_and_recipient_cancel(self):
+        sender_go_vesting_select_devnet_connect_wallet()
+        sender_fill_standard_contract_details()
+        sender_use_random_date_and_time()
+        select_both_can_cancel()
+        sender_create_contract_and_recipient_assert_contract_vesting()
+        cancel_contract()
 
-    # def test_streaming_hours(self):
-    #     sender_go_streaming_select_devnet_connect_wallet()
-    #     sender_use_hours()
-    #     fill_standard_details_for_streaming()
-    #     sender_create_contract_and_recipient_assert_contract_streaming()
+    def test_vesting_and_sender_set_cliff(self):
+        sender_go_vesting_select_devnet_connect_wallet()
+        sender_fill_standard_contract_details()
+        sender_use_random_date_and_time()
+        set_random_cliff()
+        sender_create_contract_and_recipient_assert_contract_vesting()
 
-    # def test_vesting_sender_select_autowithdraw(self):
-    #     sender_go_vesting_select_devnet_connect_wallet()
-    #     sender_fill_standard_contract_details()
-    #     sender_select_autowithdrawal()
-    #     sender_create_contract_and_recipient_assert_contract_vesting()
-    #
-    # def test_vesting_sender_big_number(self):
-    #     sender_go_vesting_select_devnet_connect_wallet()
-    #     sender_fill_big_amount_contract_details()
-    #     sender_create_contract_and_recipient_assert_contract_vesting()
+    def test_streaming_hours(self):
+        sender_go_streaming_select_devnet_connect_wallet()
+        sender_use_hours()
+        fill_standard_details_for_streaming()
+        sender_create_contract_and_recipient_assert_contract_streaming()
+
+    def test_vesting_sender_select_autowithdraw(self):
+        sender_go_vesting_select_devnet_connect_wallet()
+        sender_fill_standard_contract_details()
+        sender_select_autowithdrawal()
+        sender_create_contract_and_recipient_assert_contract_vesting()
+
+    def test_vesting_sender_big_number(self):
+        sender_go_vesting_select_devnet_connect_wallet()
+        sender_fill_big_amount_contract_details()
+        sender_create_contract_and_recipient_assert_contract_vesting()
 
     @classmethod
     def tearDown(cls):
