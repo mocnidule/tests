50b2c60cec8d4f1205d8759051013d6ea33caf50
Dusan Kovacevic <dusankovacevic@Dusans-MacBook-Pro-2.local>
2022-07-24 20:09:52 +0200

    remove password

--- tests/Transactions/test_enviorment.py
+++ tests/Transactions/test_enviorment.py
@@ -6,7 +6,7 @@ from helpers.app_helpers import use_minutes, \
     select_both_can_cancel, \
     create_vesting_contract_and_assert, sender_create_payment_contract, find_contract_and_assert, select_devnet, \
     use_months, transfer_contract, select_both_can_transfer, fill_standard_details_for_streaming, \
-    click_on_payment_tab, enter_password_and_submit
+    click_on_payment_tab, set_random_cliff
 from helpers.driver_helpers import tear_down
 from pages.solflare_wallet import connect_sender_wallet, connect_recipient_wallet
 
@@ -14,13 +14,13 @@ from pages.solflare_wallet import connect_sender_wallet, connect_recipient_walle
 @pytest.mark.smoke
 @flaky(max_runs=1, min_passes=1)
 def test_vesting_months_cliff_and_cancel(setup):
-    enter_password_and_submit()
     connect_sender_wallet()
     select_devnet()
     use_months()
     select_both_can_cancel()
     enter_standard_contract_details()
     use_random_date_and_time()
+    set_random_cliff()
     create_vesting_contract_and_assert()
     connect_recipient_wallet()
     select_devnet()
@@ -32,7 +32,6 @@ def test_vesting_months_cliff_and_cancel(setup):
 @pytest.mark.smoke
 @flaky(max_runs=1, min_passes=1)
 def test_streaming_minutes_top_up_and_transfer(setup):
-    enter_password_and_submit()
     connect_sender_wallet()
     click_on_payment_tab()
     select_devnet()
