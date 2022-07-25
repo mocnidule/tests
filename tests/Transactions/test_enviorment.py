0e04a216f1888d7611f0a762892e44416c196248
Dusan Kovacevic <dusankovacevic@Dusans-MacBook-Pro-2.local>
2022-07-25 16:56:00 +0200

    increase runs

--- tests/Transactions/test_enviorment.py
+++ tests/Transactions/test_enviorment.py
@@ -6,13 +6,14 @@ from helpers.app_helpers import use_minutes, \
     select_both_can_cancel, \
     create_vesting_contract_and_assert, sender_create_payment_contract, find_contract_and_assert, select_devnet, \
     use_months, transfer_contract, select_both_can_transfer, fill_standard_details_for_streaming, \
-    click_on_payment_tab
+    click_on_payment_tab, enter_password_and_submit
 from pages.solflare_wallet import connect_sender_wallet, connect_recipient_wallet
 
 
 @pytest.mark.smoke
-@flaky(max_runs=1, min_passes=1)
+@flaky(max_runs=3, min_passes=1)
 def test_vesting_months_cliff_and_cancel(setup):
+    enter_password_and_submit()
     connect_sender_wallet()
     select_devnet()
     use_months()
@@ -27,8 +28,9 @@ def test_vesting_months_cliff_and_cancel(setup):
 
 
 @pytest.mark.smoke
-@flaky(max_runs=1, min_passes=1)
+@flaky(max_runs=3, min_passes=1)
 def test_streaming_minutes_top_up_and_transfer(setup):
+    enter_password_and_submit()
     connect_sender_wallet()
     click_on_payment_tab()
     select_devnet()
