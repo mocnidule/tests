03ee15e44e5405ab3c07d59371aa48f63a2a0649
Dusan Kovacevic <dusankovacevic@Dusans-MacBook-Pro-2.local>
2023-01-29 11:00:30 +0100

    fix

--- pages/wallet_page.py
+++ pages/wallet_page.py
@@ -69,6 +69,7 @@ def connect_sender_wallet_on_solana():
     # except TypeError:
     #     handle_second_window()
     #     click(driver.instance, By.XPATH, already_have_wallet_button)
+    handle_default_window()
     handle_new_window()
     click(driver.instance, By.XPATH, already_have_wallet_button)
     enter_mnemonic_solana()
